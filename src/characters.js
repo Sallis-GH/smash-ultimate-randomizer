ad4c77d128b1d76c55739451fda8228514b29d7c
Sallis-GH <74314359+Sallis-GH@users.noreply.github.com>
2024-03-27 11:50:04 +0100

    MVP done

---
+++ src/characters.js
@@ -0,0 +1,430 @@
+import BanjooAndKazooie from "./assets/characters/Banjoo & Kazoie.png"
+import Bayonetta from "./assets/characters/Bayonetta.png"
+import BowserJr from "./assets/characters/Bowser Jr.png"
+import Bowser from "./assets/characters/Bowser.png"
+import Byleth from "./assets/characters/Byleth.png"
+import CaptainFalcon from "./assets/characters/Captain Falcon.png"
+import Chrom from "./assets/characters/Chrom.png"
+import Cloud from "./assets/characters/Cloud.png"
+import Corrin from "./assets/characters/Corrin.png"
+import Daisy from "./assets/characters/Daisy.png"
+import DarkPit from "./assets/characters/Dark Pit.png"
+import DarkSamus from "./assets/characters/Dark Samus.png"
+import DiddyKong from "./assets/characters/Diddy Kong.png"
+import DonkeyKong from "./assets/characters/Donkey Kong.png"
+import DrMario from "./assets/characters/Dr. Mario.png"
+import Duckhunt from "./assets/characters/Duckhunt.png"
+import Falco from "./assets/characters/Falco.png"
+import Fox from "./assets/characters/Fox.png"
+import GameAndWatch from "./assets/characters/Game & Watch.png"
+import Ganondorf from "./assets/characters/Ganondorf.png"
+import Greninja from "./assets/characters/Greninja.png"
+import Hero from "./assets/characters/Hero.png"
+import IceClimbers from "./assets/characters/Ice Climbers.png"
+import Ike from "./assets/characters/Ike.png"
+import Incineroar from "./assets/characters/Incineroar.png"
+import Inkling from "./assets/characters/Inkling.png"
+import Isabelle from "./assets/characters/Isabelle.png"
+import Jigglypuff from "./assets/characters/Jigglypuff.png"
+import Joker from "./assets/characters/Joker.png"
+import Kazuya from "./assets/characters/Kazuya.png"
+import Ken from "./assets/characters/Ken.png"
+import KingDedede from "./assets/characters/King Dedede.png"
+import KingKRool from "./assets/characters/King K. Rool.png"
+import Kirby from "./assets/characters/Kirby.png"
+import Link from "./assets/characters/Link.png"
+import LittleMac from "./assets/characters/Little Mac.png"
+import Lucario from "./assets/characters/Lucario.png"
+import Lucas from "./assets/characters/Lucas.png"
+import Lucina from "./assets/characters/Lucina.png"
+import Luigi from "./assets/characters/Luigi.png"
+import Mario from "./assets/characters/Mario.png"
+import Marth from "./assets/characters/Marth.png"
+import Megaman from "./assets/characters/Megaman.png"
+import MetaKnight from "./assets/characters/Meta Knight.png"
+import Mewtwo from "./assets/characters/Mewtwo.png"
+import MiiFighter from "./assets/characters/Mii Fighter.png"
+import MiiGunner from "./assets/characters/Mii Gunner.png"
+import MiiSwordsman from "./assets/characters/Mii Swordsman.png"
+import MinMin from "./assets/characters/Min Min.png"
+import PyraAndMythra from "./assets/characters/Pyra & Mythra.png"
+import Ness from "./assets/characters/Ness.png"
+import Olimar from "./assets/characters/Olimar.png"
+import Pacman from "./assets/characters/Pacman.png"
+import Palutena from "./assets/characters/Palutena.png"
+import Peach from "./assets/characters/Peach.png"
+import Pichu from "./assets/characters/Pichu.png"
+import Pikachu from "./assets/characters/Pikachu.png"
+import PiranhaPlant from "./assets/characters/Piranha Plant.png"
+import Pit from "./assets/characters/Pit.png"
+import PokemonTrainer from "./assets/characters/Pokemon Trainer.png"
+import Rob from "./assets/characters/R.O.B.png"
+import Richter from "./assets/characters/Richter.png"
+import Ridley from "./assets/characters/Ridley.png"
+import Robin from "./assets/characters/Robin.png"
+import Roselina from "./assets/characters/Roselina.png"
+import Roy from "./assets/characters/Roy.png"
+import Ryu from "./assets/characters/Ryu.png"
+import Samus from "./assets/characters/Samus.png"
+import Sephiroth from "./assets/characters/Sephiroth.png"
+import Sheik from "./assets/characters/Sheik.png"
+import Shulk from "./assets/characters/Shulk.png"
+import Simon from "./assets/characters/Simon.png"
+import Snake from "./assets/characters/Snake.png"
+import Sonic from "./assets/characters/Sonic.png"
+import Steve from "./assets/characters/Steve.png"
+import Terry from "./assets/characters/Terry.png"
+import ToonLink from "./assets/characters/Toon Link.png"
+import Villager from "./assets/characters/Villager.png"
+import Wario from "./assets/characters/Wario.png"
+import WiiFitTrainer from "./assets/characters/Wii Fit Trainer.png"
+import Wolf from "./assets/characters/Wolf.png"
+import Yoshi from "./assets/characters/Yoshi.png"
+import YoungLink from "./assets/characters/Young Link.png"
+import Zelda from "./assets/characters/Zelda.png"
+import ZeroSuitSamus from "./assets/characters/Zero Suit Samus.png"
+
+const characters = [
+  {
+    name: 'Banjoo & Kazooie',
+    image: BanjooAndKazooie,
+  },
+  {
+    name: 'Bayonetta',
+    image: Bayonetta,
+  },
+  {
+    name: 'Bowser Jr',
+    image: BowserJr,
+  },
+  {
+    name: 'Bowser',
+    image: Bowser,
+  },
+  {
+    name: 'Byleth',
+    image: Byleth,
+  },
+  {
+    name: 'Captain Falcon',
+    image: CaptainFalcon,
+  },
+  {
+    name: 'Chrom',
+    image: Chrom,
+  },
+  {
+    name: 'Cloud',
+    image: Cloud,
+  },
+  {
+    name: 'Corrin',
+    image: Corrin,
+  },
+  {
+    name: 'Daisy',
+    image: Daisy,
+  },
+  {
+    name: 'Dark Pit',
+    image: DarkPit,
+  },
+  {
+    name: 'Dark Samus',
+    image: DarkSamus,
+  },
+  {
+    name: 'Diddy Kong',
+    image: DiddyKong,
+  },
+  {
+    name: 'Donkey Kong',
+    image: DonkeyKong,
+  },
+  {
+    name: 'Dr. Mario',
+    image: DrMario,
+  },
+  {
+    name: 'Duckhunt',
+    image: Duckhunt,
+  },
+  {
+    name: 'Falco',
+    image: Falco,
+  },
+  {
+    name: 'Fox',
+    image: Fox,
+  },
+  {
+    name: 'Game & Watch',
+    image: GameAndWatch,
+  },
+  {
+    name: 'Ganondorf',
+    image: Ganondorf,
+  },
+  {
+    name: 'Greninja',
+    image: Greninja,
+  },
+  {
+    name: 'Hero',
+    image: Hero,
+  },
+  {
+    name: 'Ice Climbers',
+    image: IceClimbers,
+  },
+  {
+    name: 'Ike',
+    image: Ike,
+  },
+  {
+    name: 'Incineroar',
+    image: Incineroar,
+  },
+  {
+    name: 'Inkling',
+    image: Inkling,
+  },
+  {
+    name: 'Isabelle',
+    image: Isabelle,
+  },
+  {
+    name: 'Jigglypuff',
+    image: Jigglypuff,
+  },
+  {
+    name: 'Joker',
+    image: Joker,
+  },
+  {
+    name: 'Kazuya',
+    image: Kazuya,
+  },
+  {
+    name: 'Ken',
+    image: Ken,
+  },
+  {
+    name: 'King Dedede',
+    image: KingDedede,
+  },
+  {
+    name: 'King K. Rool',
+    image: KingKRool,
+  },
+  {
+    name: 'Kirby',
+    image: Kirby,
+  },
+  {
+    name: 'Link',
+    image: Link,
+  },
+  {
+    name: 'Little Mac',
+    image: LittleMac,
+  },
+  {
+    name: 'Lucario',
+    image: Lucario,
+  },
+  {
+    name: 'Lucas',
+    image: Lucas,
+  },
+  {
+    name: 'Lucina',
+    image: Lucina,
+  },
+  {
+    name: 'Luigi',
+    image: Luigi,
+  },
+  {
+    name: 'Mario',
+    image: Mario,
+  },
+  {
+    name: 'Marth',
+    image: Marth,
+  },
+  {
+    name: 'Megaman',
+    image: Megaman,
+  },
+  {
+    name: 'Meta Knight',
+    image: MetaKnight,
+  },
+  {
+    name: 'Mewtwo',
+    image: Mewtwo,
+  },
+  {
+    name: 'Mii Fighter',
+    image: MiiFighter,
+  },
+  {
+    name: 'Mii Gunner',
+    image: MiiGunner,
+  },
+  {
+    name: 'Mii Swordsman',
+    image: MiiSwordsman,
+  },
+  {
+    name: 'Min Min',
+    image: MinMin,
+  },
+  {
+    name: 'Pyra & Mythra',
+    image: PyraAndMythra,
+  },
+  {
+    name: 'Ness',
+    image: Ness,
+  },
+  {
+    name: 'Olimar',
+    image: Olimar,
+  },
+  {
+    name: 'Pacman',
+    image: Pacman,
+  },
+  {
+    name: 'Palutena',
+    image: Palutena,
+  },
+  {
+    name: 'Peach',
+    image: Peach,
+  },
+  {
+    name: 'Pichu',
+    image: Pichu,
+  },
+  {
+    name: 'Pikachu',
+    image: Pikachu,
+  },
+  {
+    name: 'Piranha Plant',
+    image: PiranhaPlant,
+  },
+  {
+    name: 'Pit',
+    image: Pit,
+  },
+  {
+    name: 'Pokemon Trainer',
+    image: PokemonTrainer,
+  },
+  {
+    name: 'R.O.B',
+    image: Rob,
+  },
+  {
+    name: 'Richter',
+    image: Richter,
+  },
+  {
+    name: 'Ridley',
+    image: Ridley,
+  },
+  {
+    name: 'Robin',
+    image: Robin,
+  },
+  {
+    name: 'Roselina',
+    image: Roselina,
+  },
+  {
+    name: 'Roy',
+    image: Roy,
+  },
+  {
+    name: 'Ryu',
+    image: Ryu,
+  },
+  {
+    name: 'Samus',
+    image: Samus,
+  },
+  {
+    name: 'Sephiroth',
+    image: Sephiroth,
+  },
+  {
+    name: 'Sheik',
+    image: Sheik,
+  },
+  {
+    name: 'Shulk',
+    image: Shulk,
+  },
+  {
+    name: 'Simon',
+    image: Simon,
+  },
+  {
+    name: 'Snake',
+    image: Snake,
+  },
+  {
+    name: 'Sonic',
+    image: Sonic,
+  },
+  {
+    name: 'Steve',
+    image: Steve,
+  },
+  {
+    name: 'Terry',
+    image: Terry,
+  },
+  {
+    name: 'Toon Link',
+    image: ToonLink,
+  },
+  {
+    name: 'Villager',
+    image: Villager,
+  },
+  {
+    name: 'Wario',
+    image: Wario,
+  },
+  {
+    name: 'Wii Fit Trainer',
+    image: WiiFitTrainer,
+  },
+  {
+    name: 'Wolf',
+    image: Wolf,
+  },
+  {
+    name: 'Yoshi',
+    image: Yoshi,
+  },
+  {
+    name: 'Young Link',
+    image: YoungLink,
+  },
+  {
+    name: 'Zelda',
+    image: Zelda,
+  },
+  {
+    name: 'Zero Suit Samus',
+    image: ZeroSuitSamus,
+  },
+];
+
+export default characters;
--- src/views/Ironman.jsx
+++ src/views/Ironman.jsx
@@ -1,37 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Button from '../components/Button';
-
-const tempCharacters = [
-  {
-    name: 'Mario',
-    image:
-      'https://pm1.aminoapps.com/6915/cbe3f944fca315ae7da44af16d787c601d16f67ar1-660-660v2_uhq.jpg',
-  },
-  {
-    name: 'Link',
-    image:
-      'https://static.wikia.nocookie.net/cpucs-tournament/images/7/75/LinkIcon.png/revision/latest?cb=20190209011432',
-  },
-  {
-    name: 'Samus',
-    image:
-      'https://ih1.redbubble.net/image.4091914591.1297/st,small,845x845-pad,1000x1000,f8f8f8.jpg',
-  },
-];
+import characters from '../characters';
 
 const Ironman = () => {
-  const [characters, setCharacters] = useState(tempCharacters);
-  const [characterList, setCharacterList] = useState(tempCharacters);
+  const [characterList, setCharacterList] = useState(characters.sort(() => Math.random() - 0.5));
   const randomNumber = Math.floor(Math.random() * characterList.length);
   const [currentCharacter, setCurrentCharacter] = useState(
     characters[randomNumber]
   );
-
-  useEffect(() => {
-    setCharacters(tempCharacters);
-    setCharacterList(characters.sort(() => Math.random() - 0.5));
-  }, [characters]);
-
   const handleNextRoll = () => {
     const newList  = characterList.filter((character) => character.name !== currentCharacter.name)
     setCharacterList(newList);
@@ -39,20 +15,20 @@ const Ironman = () => {
   };
   const resetRandomizer = () => {
     const newList = characters.sort(() => Math.random() - 0.5)
-    console.table(newList)
     setCharacterList(newList);
     setCurrentCharacter(newList[0]);
   };
-
   return (
     <>
       <div className='gap-2 flex flex-col items-center justify-center h-full'>
         <h1>Ironman Challenge</h1>
+        <img src={currentCharacter?.image} alt={currentCharacter?.name} />
         {characterList.length > 0 ? <h2>{currentCharacter?.name}</h2> : <h2>You Win!</h2>}
         <div className='flex gap-2'>
           <Button title={'Win'} onClick={handleNextRoll} />
           <Button title={'Reset'} onClick={resetRandomizer} />
         </div>
+        <h2>Characters Left: {characterList.length}/{characters.length}</h2>
       </div>
     </>
   );
